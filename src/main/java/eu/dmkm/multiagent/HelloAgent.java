adfff5ca1fec9ce404f18ac8b8caf9bae395d4a2
krishnakalyan3 <krishnakalyan3@gmail.com>
2016-02-06 17:20:56 +0100

    Commit Date Sat Feb  6 17:20:55 CET 2016

---
+++ src/main/java/eu/dmkm/multiagent/HelloAgent.java
@@ -0,0 +1,14 @@
+package eu.dmkm.multiagent;
+
+import jade.core.Agent;
+import jade.core.Agent;
+
+
+public class HelloAgent extends Agent 
+{ 
+    protected void setup() 
+    { 
+        System.out.println(getLocalName()); 
+    }
+}
+
